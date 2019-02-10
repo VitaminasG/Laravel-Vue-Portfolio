792bed4bcc7e459c38c35f4f606b2dc0d90cabf2
Gediminas Palsys <vitaminas.g@gmail.com>
2019-02-10 01:31:19 +0000

    webPack set to Production
    Signed-off-by: Gediminas Palsys <vitaminas.g@gmail.com>

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,8 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/mobile.js', 'public/js/mobile')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/mobileApp.scss', 'public/css/mobile')
-    .version();
+    .sass('resources/sass/mobileApp.scss', 'public/css/mobile');
+
+if (mix.inProduction()) {
+    mix.version();
+}
