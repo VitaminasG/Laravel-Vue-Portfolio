d0db8b19ea666ed437fa24b59c16ceb56e1b33f6
Gediminas Palsys <vitaminas.g@gmail.com>
2019-04-29 19:31:45 +0100

    Back API:Throttle for Login, Register Routes
    Signed-off-by: Gediminas Palsys <vitaminas.g@gmail.com>

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 Route::get('/verify', 'API\ApiController@verify');
-Route::post('/login', 'API\ApiController@login');
-Route::post('/register', 'API\ApiController@register');
 Route::get('/register', 'API\ApiController@register');
+
+/*
+ * TODO : For Production set throttle on ':10,1' - 10 request per 1 min.
+ */
+
+Route::post('/register', 'API\ApiController@register')->middleware('throttle');
+Route::post('/login', 'API\ApiController@login')->middleware('throttle');
 
 Route::middleware('auth:api')->group(function(){
 
